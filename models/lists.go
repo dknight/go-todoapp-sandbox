cb47ee233537d2b6c9d590e9588fc50b096595cb
Dmitri Smirnov <smirnov.dmitri@gmail.com>
2023-03-06 14:15:16 +0200

    CRUD for lists.

--- models/lists.go
+++ models/lists.go
@@ -5,32 +5,86 @@ import (
 	"errors"
 )
 
+var (
+	ErrListNameEmpty    = errors.New("List name is empty")
+	ErrListNotFound     = errors.New("List not found")
+	ErrCannotDeleteList = errors.New("Cannot delete the list")
+)
+
 type List struct {
 	ID   int    `db:"id"`
 	Name string `db:"name"`
 }
 
-// TODO
 func ListLists(db *sql.DB) ([]Lists, error) {
-	return nil, errors.New("")
+	var lists []Lists
+	rows, err := db.Query(`SELECT id, name FROM lists;`)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		list := List{}
+		err := rows.Scan(&list.ID, &list.Name)
+		if err != nil {
+			return nil, err
+		}
+		lists = append(lists, list)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+	return items, nil
 }
 
-// TODO
-func FindList(db *sql.DB, id int) (Lists, error) {
-	return nil, errors.New("")
+func FindList(db *sql.DB, id int) (List, error) {
+	list := List{}
+	row := db.QueryRow(`
+		SELECT id, name
+		FROM lists
+		WHERE id = $1`, id)
+	if err := row.Err(); err != nil {
+		return nil, err
+	}
+
+	err := row.Scan(&list.ID, &list.Name)
+	if err != nil {
+		return nil, err
+	}
+	return &list, nil
 }
 
-// TODO
 func (li List) Create(db *sql.DB) (int64, error) {
-	return 0, errors.New("")
+	var id int64
+	if ti.Name != "" {
+		// Postgres doesnt support LastInsertedID(), so we user QueryRow,
+		// instead of Exec.
+		err := db.QueryRow(
+			`INSERT INTO lists (name) VALUES ($1) RETURNING id`,
+			li.Task).Scan(&id)
+		if err != nil {
+			return 0, err
+		}
+		return id, nil
+	}
+	return 0, ErrListNameEmpty
 }
 
-// TODO
-func (li List) Save(db *sql.DB) (int64, error) {
-	return 0, errors.New("")
+func (li List) Save(db *sql.DB) error {
+	_, err := db.Exec(`UPDATE lists SET name=$1 WHERE id=$2`,
+		li.Name, li.ID)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
-// TODO
-func (li List) Delete(db *sql.DB) (int64, error) {
-	return 0, errors.New("")
+func (li List) Delete(db *sql.DB) error {
+	_, err := db.Exec(`DELETE FROM lists WHERE id=$1`, li.ID)
+	if err != nil {
+		return ErrCannotDeleteList
+	}
+	return nil
 }
